aa1739ae0e89114082da11c0d1b8256efe7663da
Ragunanthan <ragunanthan0203@gmail.com>
2026-07-14 19:32:51 +0530

    Added Shipping_label

---
+++ shipping label/Shipping_label.py
@@ -0,0 +1,47 @@
+#Shipping label program
+def shipping_label(*args,**kwargs):
+    for arg in args:
+        print(arg,end=' ')
+    print()
+    if "apt" in kwargs :
+        print(f"{kwargs.get('apt')}",end=',')
+        print()
+        print(f"{kwargs.get('street')}",end=',')
+        print()
+        print(f"{kwargs.get('state')}",end=',')
+        print()
+        print(f"{kwargs.get('country')}",end='.')
+    elif "House_no" in kwargs:
+        print(f"{kwargs.get('House_no')}",end=',')
+        print()
+        print(f"{kwargs.get('street')}",end=',')
+        print()
+        print(f"{kwargs.get('state')}",end=',')
+        print()
+        print(f"{kwargs.get('country')}",end='.')
+
+    else:
+ 
+        print(f"{kwargs.get('street')}",end=',')
+        print()
+        print(f"{kwargs.get('state')}",end=',')
+        print()
+        print(f"{kwargs.get('country')}",end='.')
+
+#Sample input
+shipping_label("Mr.","Ragunanthan",
+               House_no=100,
+               street="Boston street",
+               state="Boston",
+               country="USA"
+               )
+        
+#Output:
+
+"""
+Mr. Ragunanthan
+100,
+Boston street,
+Boston,
+USA.
+"""
